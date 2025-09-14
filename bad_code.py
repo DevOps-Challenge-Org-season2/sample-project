b24b1d6bf9c0348ec6913f72bf7d5fa3eccf8050
ManjunathReddy9398 <manjunathreddy9398@gmail.com>
2025-09-14 10:45:56 +0530

    Create bad_code.py

---
+++ bad_code.py
@@ -0,0 +1,12 @@
+import os,sys   # bad practice: multiple imports on one line
+
+def add_numbers(a,b):  # missing spaces after comma
+  result=a+b     # bad indentation + no proper spacing
+  print("Sum is:",result)   # print inside function (ok, but will trigger style/lint warnings)
+  return result
+
+def unused_function():
+    x = 42   # unused variable
+    return
+
+add_numbers(2,3)
